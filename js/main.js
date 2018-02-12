296bdbb49b5fb5aed41f85e8abb033d820028140
dargavavid <dargavavid@gmail.com>
2018-02-12 21:09:39 +0100

    Add function to make one dimensional grid of specified size

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+// Generate array of numbers within specified range
 function range(a, b) {
     return new Array(b - a + 1).fill(0).map((_, i) => i + a);
+}
+
+// Make one dimensional grid of specified size
+function makeFlatGrid(rows, columns) {
+    return new Array(rows * columns).fill(0);
 }
